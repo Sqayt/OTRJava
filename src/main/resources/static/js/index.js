0f97ac83cbc312f6be67ed9f302bf7f2da890deb
SqGoliaf <ivanov@v-1336.ru>
2022-11-19 09:29:48 +0300

    Начал front end разработку

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import Tab from "@material-ui/core/Tab";
+import Tabs from "@material-ui/core/Tabs";
+
+function Tabs() {
+    return (
+        <div className="Tabs">
+            <div className="tabsList">
+                <div className="tabHead">
+
+                </div>
+                <div className="tabHead">
+
+                </div>
+                <div className="tabHead">
+
+                </div>
+                <div className="tabHead">
+
+                </div>
+            </div>
+            <div className="tabContact"></div>
+            <div className="tabContact"></div>
+            <div className="tabContact"></div>
+
+        </div>
+    )
+}
